288cee3eec771f2199f83acf48046a5801346eaa
Khanh <khanhbinh6@yahoo.com>
2024-06-05 16:14:49 +0700

    update 4

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 import datetime
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 def timestamp():
     ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -17,9 +19,9 @@ def login(user, password):
     print(timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     # login
-    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
-    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
-    driver.find_element_by_id("login-button").click()
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)   
+    driver.find_element(By.ID, 'login-button').click() 
     print(timestamp() + 'Login with username {:s} and password {:s} successfully.'.format(user, password))
     return driver
 
@@ -27,46 +29,49 @@ def add_cart(driver, n_items):
     print (timestamp() +'Test: adding items to cart')
     for i in range(n_items):
         element = "a[id='item_" + str(i) + "_title_link']"  
-        driver.find_element_by_css_selector(element).click()  
-        driver.find_element_by_css_selector("button.btn_primary.btn_inventory").click()  
-        product = driver.find_element_by_css_selector('.inventory_details_name.large_size').text  
-        print(timestamp() + product + " added to shopping cart.") 
-        driver.find_element_by_css_selector("button.inventory_details_back_button").click()  
+        driver.find_element(By.CSS_SELECTOR, element).click() 
+        driver.find_element(By.CSS_SELECTOR, "button.btn_primary.btn_inventory").click() 
+        product= driver.find_element(By.CSS_SELECTOR, '.inventory_details_name.large_size').text
+        print(timestamp() + product + " added to shopping cart.")  
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
     print(timestamp() + '{:d} items are all added to shopping cart successfully.'.format(n_items))
 
 def remove_cart(driver, n_items):
     for i in range(n_items):
         element = "a[id='item_" + str(i) + "_title_link']"
-        driver.find_element_by_css_selector(element).click()
-        driver.find_element_by_css_selector("button.btn_secondary.btn_inventory").click()
-        product = driver.find_element_by_css_selector('.inventory_details_name.large_size').text
+        driver.find_element(By.CSS_SELECTOR, element).click()
+        driver.find_element(By.CSS_SELECTOR, "button.btn_secondary.btn_inventory").click()
+        product = driver.find_element(By.CSS_SELECTOR, '.inventory_details_name.large_size').text
+        
         print(timestamp() + product + " removed from shopping cart.") 
-        driver.find_element_by_css_selector("button.inventory_details_back_button").click()
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click()
     print(timestamp() + '{:d} items are all removed from shopping cart successfully.'.format(n_items))
 
 def add_cart_check(driver, n_items):
     print (timestamp() +'Test: adding items to cart for check out')
     for i in range(n_items):
-        element = "a[id='item_" + str(i) + "_title_link']"  
-        driver.find_element_by_css_selector(element).click()  
-        driver.find_element_by_css_selector("button.btn_primary.btn_inventory").click() 
-        product = driver.find_element_by_css_selector('.inventory_details_name.large_size').text  
+        element = "a[id='item_" + str(i) + "_title_link']"    
+        driver.find_element(By.CSS_SELECTOR, element).click()
+        driver.find_element(By.CSS_SELECTOR, "button.btn_primary.btn_inventory").click()
+        product = driver.find_element(By.CSS_SELECTOR, '.inventory_details_name.large_size').text
+        
         print(timestamp() + product + " added to shopping cart.")  
-        driver.find_element_by_css_selector("button.inventory_details_back_button").click()  
+        driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button").click() 
     print(timestamp() + '{:d} items are all added to shopping cart successfully ready checkout.'.format(n_items))
 
 def check_out(driver):
-    driver.get('https://www.saucedemo.com/inventory.html')
-    driver.find_element_by_css_selector('.shopping_cart_badge').click()  
-    driver.find_element_by_css_selector('#checkout').click() 
-    driver.find_element_by_css_selector("input[id='first-name']").send_keys('Anh')
-    driver.find_element_by_css_selector("input[id='last-name']").send_keys('Pham')
-    driver.find_element_by_css_selector("input[id='postal-code']").send_keys('11111')
-    driver.find_element_by_css_selector('#continue').click() 
-    driver.find_element_by_css_selector('#finish').click() 
-    status_check = driver.find_element_by_css_selector('.complete-header').text
+    driver.get('https://www.saucedemo.com/inventory.html')   
+    driver.find_element(By.CSS_SELECTOR, '.shopping_cart_badge').click()
+    driver.find_element(By.CSS_SELECTOR, '#checkout').click()
+    driver.find_element(By.CSS_SELECTOR, "input[id='first-name']").send_keys('Khanh')
+    driver.find_element(By.CSS_SELECTOR, "input[id='last-name']").send_keys('Doan')
+    driver.find_element(By.CSS_SELECTOR, "input[id='postal-code']").send_keys('11111')
+    driver.find_element(By.CSS_SELECTOR, '#continue').click()
+    driver.find_element(By.CSS_SELECTOR, '#finish').click()
+    status_check = driver.find_element(By.CSS_SELECTOR, '.complete-header').text
+        
     print(timestamp() + status_check + " .Your order has been dispatched, and will arrive just as fast as the pony can get there!")
-    driver.find_element_by_css_selector('#back-to-products').click()  
+    driver.find_element(By.CSS_SELECTOR, '#back-to-products').click()  
 
 if __name__ == "__main__":
     N_ITEMS = 6
